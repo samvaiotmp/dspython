bd346ca9b2711185abe36ef822ada6b45af241f2
Shaf <warnergears@gmail.com>
2019-01-30 23:10:31 -0600

    first comit

---
+++ binary_search.py
@@ -0,0 +1,44 @@
+def binary_search(list, target):
+    first = 0
+    last = len(list) - 1
+
+    while first <= last:
+        midpoint = (first + last)//2
+
+        if list[midpoint] == target:
+            return midpoint
+        elif list[midpoint] < target:
+            first = midpoint + 1
+        else:
+            last = midpoint - 1
+
+    return None
+
+def recursive_binary_search(list, target):
+    if len(list) == 0:
+        return False
+    else:
+        midpoint = (len(list))//2
+        if list[midpoint] == target:
+            return True
+        else:
+            if list[midpoint] < target:
+                return recursive_binary_search(list[midpoint+1:], target)
+            else:
+                return recursive_binary_search(list[:midpoint], target)
+
+def verify(index):
+    if index is not None:
+        print("Target found at index: ", index)
+    else:
+        print("Target not found in list")
+
+def verify_rec(result):
+    print("Target found: ", result)
+
+
+
+numbers = [1,2,3,4,5,6,7,8,9,10]
+
+result = recursive_binary_search(numbers, 13)
+verify(result)
