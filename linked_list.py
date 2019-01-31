e5c815de035cd5770c2c8f8c89c874ee5fd3224c
Shaf <warnergears@gmail.com>
2019-01-31 00:32:56 -0600

    done home 01312019

--- linked_list.py
+++ linked_list.py
@@ -4,4 +4,23 @@ class Node:
 
     def __init__(self, data):
         self.data = data
-        
+
+    def __repr__(self):
+        return "<Node data : %s>" % self.data
+
+class LinkedList:
+
+    def __init__(self):
+        self.head = None
+    
+    def is_empty(self):
+        return self.head == None
+
+    def size(self):
+        current = self.head
+        count = 0
+        while current:
+            count += 1
+            current = current.next_node
+
+        return count
